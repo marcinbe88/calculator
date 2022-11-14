2c5bfebf89a435f75a2b70d1e902e2f8c6744006
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-14 15:01:47 +0100

    calculator-app basic view ready

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+const addButton = document.getElementById("add")
+const subtractButton = document.getElementById("subtract")
+const multiplyButton = document.getElementById("multiply")
+const divideButton = document.getElementById("divide")
+
+const inputNumber = document.getElementById("number-box")
+
+const result = document.getElementById("result")
+
+result.append('hi')
+
+let initialValue = 0
+
+function add() {
+    
+}
+
+
+addEventListener
