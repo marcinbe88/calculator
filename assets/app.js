1d809b89be25fade6b1faf860d38d18621299f58
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-19 19:48:18 +0100

    calculator FINISHED

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,5 @@
 const defaultResult = 0
 let currentResult = defaultResult
-let logEntries = []
 
 function getUserNumberInput() {
   return parseInt(userInputNumber.value)
@@ -11,28 +10,11 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription)
 }
 
-function writeToLog(
-  operationIdentifier, 
-  prevResult, 
-  operationNumber, 
-  newResult
-) {
-  const logEntry = {
-    operation: operationIdentifier,
-    prevResult: prevResult,
-    number: operationNumber,
-    result: newResult
-  }
-  logEntries.push(logEntry)
-  console.log(logEntries)
-}
-
 function add() {
   const enteredNumber = getUserNumberInput()
   const initialResult = currentResult
   currentResult += enteredNumber
   createAndWriteOutput('+', initialResult, enteredNumber)
-  writeToLog('ADD', initialResult, enteredNumber, currentResult)
   userInputNumber.value = 0
 }
 
@@ -41,11 +23,24 @@ function subtract() {
     const initialResult = currentResult
     currentResult -= enteredNumber
     createAndWriteOutput('-', initialResult, enteredNumber)
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
     userInputNumber.value = 0
 }
 
+function multiply() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult *= enteredNumber
+    createAndWriteOutput("*", initialResult, enteredNumber)
+    userInputNumber.value = 0
+}
 
+function divide() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult /= enteredNumber
+    createAndWriteOutput("/", initialResult, enteredNumber)
+    userInputNumber.value = 0
+}
 
 addButton.addEventListener('click', add)
 subtractButton.addEventListener('click', subtract)
--- assets/vendor.js
+++ assets/vendor.js
@@ -5,8 +5,8 @@ const subtractButton = document.getElementById("subtract-button")
 const multiplyButton = document.getElementById("multiply-button")
 const divideButton = document.getElementById("divide-button")
 
-const resultOutput = document.getElementById("current-result")
 const calculationOutput = document.getElementById("current-calculation")
+const resultOutput = document.getElementById("current-result")
 
 function outputResult(result, text) {
     resultOutput.textContent = result
